632ef5d38ec7518e66a52453c417adaf480743be
Samantha Butters <61861742+snbutters@users.noreply.github.com>
2020-05-03 02:54:41 -0700

    add branching in scripts.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
   $("form#languages").submit(function(event) {
 
+    var experience = $("input:radio[name=experience]:checked").val();
+    var why = $("input:radio[name=why]:checked").val();
+    var quality = $("input:radio[name=quality]:checked").val();
+    var use = $("input:radio[name=use]:checked").val();
+    var apple = $("input:radio[name=apple]:checked").val();
 
-
-
+    if (use === "software" && quality ==="ease") {
+      $('#python, #java, #cPlusPlus, #swift').hide();
+      $('#python').show();
+    } else if (use === "software" && quality === "power") {
+      $('#python, #java, #cPlusPlus, #swift').hide();
+      $('#cPlusPlus').show();
+    } else if (use === "software" && quality === "longevity") {
+      $('#python, #java, #cPlusPlus, #swift').hide();
+      $('#java').show();
+    } else if (use === "video-game" && quality === "ease") {
+      $('#python, #java, #cPlusPlus, #swift').hide();
+      $('#python').show();
+    } else if (use === "video-game" && quality === "power") {
+      $('#python, #java, #cPlusPlus, #swift').hide();
+      $('#cPlusPlus').show();
+    } else if (use === "video-game" && quality === "longevity") {
+      $('#python, #java, #cPlusPlus, #swift').hide();
+      $('#java').show();
+    } else if (use === "web") {
+      $('#python, #java, #cPlusPlus, #swift').hide();
+      $('#python').show();
+    } else if (use === "mobile-app") {
+      $('#python, #java, #cPlusPlus, #swift').hide();
+      $('#java').show();
+    }
 
     event.preventDefault();
   });
